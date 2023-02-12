e88b59eadbbde0e121454e33367b23a5623c859a
Rubedo <rubedo@albedo.nigredo>
2023-02-12 10:23:30 +0100

    Added first draft

---
+++ pimpmyslack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# setup slackpkg+
+cd /tmp
+wget https://sourceforge.net/projects/slackpkgplus/files/slackpkg%2B-1.8.0-noarch-7mt.txz/download -O slackpkg+-1.8.0-noarch-7mt.txz
+installpkg slackpkg+-1.8.0-noarch-7mt.txz
+
+# setup multilib
+SLACKVER=15.0
+cd /root
+mkdir multilib
+lftp -c "open http://baar.alienbase.nl/mirrors/people/alien/multilib/ ; mirror -c -e ${SLACKVER}"
+cd ${SLACKVER}
+upgradepkg --reinstall --install-new *.t?z
+upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
